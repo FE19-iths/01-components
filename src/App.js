ef4a1de0115357766cd2d59d919c03f50b2d62a4
David Andersson <lejonmanen@gmail.com>
2020-05-18 14:03:02 +0200

    Light and dark mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [mode, setMode] = useState('light');
+
+    const handleClickLight = () => {
+        setMode('light');
+        console.log('handleClickLight körs');
+    }
+
+    console.log('App körs');
     return (
-        <div className="App">
+        <div className={'App ' + mode}>
             <header className="App-header">
                 <h1>My first React app!</h1>
             </header>
             <main>
                 <p>
-                This is not very impressing yet. But it will be...
+                Choose your preferred mode
+                </p>
+                <p>
+                    <button onClick={handleClickLight}> Light </button>
+                    <button onClick={() => setMode('dark')}> Dark </button>
                 </p>
             </main>
         </div>
