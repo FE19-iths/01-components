253e61b48cd225769a321ea6783c9fe28f26c388
David Andersson <lejonmanen@gmail.com>
2020-05-18 13:21:52 +0200

    First version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>My first React app!</h1>
+            </header>
+            <main>
+                <p>
+                This is not very impressing yet. But it will be...
+                </p>
+            </main>
+        </div>
+    );
 }
 
 export default App;
